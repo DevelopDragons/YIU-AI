34bc3423435c998fe6ce8fe02a9150310593f735
Yeseo Kim <yeseokim@Yeseoui-MacBookAir.local>
2025-01-25 21:13:12 +0900

    Initial commit

---
+++ src/main/java/devdragons/yiuServer/YiuServerApplication.java
@@ -0,0 +1,13 @@
+package devdragons.yiuServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiuServerApplication.class, args);
+	}
+
+}
