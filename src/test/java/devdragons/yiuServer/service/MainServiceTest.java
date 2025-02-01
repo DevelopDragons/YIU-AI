20cc17c5aeb70bf5a3e5d532ba6e11f9527c786d
yeseokim <callikys@naver.com>
2025-02-01 15:05:16 +0900

    test: 기존 테스트 코드 수정 및 회원가입 시 이메일 전송 관련 테스트 로직 작성

--- src/test/java/devdragons/yiuServer/service/MainServiceTest.java
+++ src/test/java/devdragons/yiuServer/service/MainServiceTest.java
@@ -9,26 +9,44 @@ import devdragons.yiuServer.dto.request.UserRequestDto;
 import devdragons.yiuServer.exception.CustomException;
 import devdragons.yiuServer.exception.ErrorCode;
 import devdragons.yiuServer.repository.UserRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Assertions;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class MainServiceTest {
-    @Autowired
-    MainService mainService;
+    @InjectMocks
+    private MainService mainService;
 
-    @Autowired
-    UserRepository userRepository;
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("회원가입 성공 테스트")
@@ -52,7 +70,7 @@ class MainServiceTest {
 
         // then
         Optional<User> user = userRepository.findById(userRequestDto.getId());
-        Assertions.assertTrue(user.isPresent());
+        assertThat(user.isPresent());
     }
 
     @Test
@@ -83,25 +101,38 @@ class MainServiceTest {
         userRequestDto1.setTrack(UserTrackCategory.SINGLE);
         userRequestDto1.setEntrance(UserEntranceCategory.FRESH);
         userRequestDto1.setProfessor("이완주");
+
+        // 첫 번째 회원가입 시에는 중복이 없으므로 false
+        when(userRepository.existsById(userRequestDto1.getId())).thenReturn(false);
         mainService.register(userRequestDto1);
 
+        // 두 번째 회원가입 시에는 중복이 발생해야 하므로 true로 설정
+        when(userRepository.existsById(userRequestDto1.getId())).thenReturn(true);
+
+        // when & then
+        CustomException customException = assertThrows(CustomException.class, () -> {
+            mainService.register(userRequestDto1);
+        });
+
+        assertThat(customException.getErrorCode()).isEqualTo(ErrorCode.DUPLICATE);
+        assertThat(customException.getErrorCode().getStatus()).isEqualTo(409);
+    }
+
+
+    @Test
+    @DisplayName("회원가입 시 이메일 전송 테스트")
+    void 회원가입_시_이메일_전송_테스트() throws Exception {
+        // given
+        String id = "202033009";
+
+        MimeMessage mimeMessage = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+
         // when
-        UserRequestDto userRequestDto = new UserRequestDto();
-        userRequestDto.setId("202033009");
-        userRequestDto.setName("김예서");
-        userRequestDto.setPwd("1234");
-        userRequestDto.setGrade(1);
-        userRequestDto.setRole(UserRoleCategory.ADMIN);
-        userRequestDto.setStatus(UserStatusCategory.STUDENT);
-        userRequestDto.setMajor("33");
-        userRequestDto.setDepartment("컴퓨터과학");
-        userRequestDto.setTrack(UserTrackCategory.SINGLE);
-        userRequestDto.setEntrance(UserEntranceCategory.FRESH);
-        userRequestDto.setProfessor("이완주");
+        int authNum = mainService.sendEmailWhenRegister(id);
 
         // then
-        CustomException customException = assertThrows(CustomException.class, () -> {mainService.register(userRequestDto);});
-        assertEquals(ErrorCode.DUPLICATE, customException.getErrorCode());
-        assertEquals(409, customException.getErrorCode().getStatus());
+        assertThat(authNum).isGreaterThanOrEqualTo(100000).isLessThanOrEqualTo(999999);
+        verify(javaMailSender, times(1)).send(any(MimeMessage.class));
     }
 }
