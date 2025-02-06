6e9ec9a46e6824067e78fda003b32f6c62b396aa
yeseokim <callikys@naver.com>
2025-02-06 22:16:55 +0900

    test: main service 관련 테스트 로직 작성

--- src/test/java/devdragons/yiuServer/service/MainServiceTest.java
+++ src/test/java/devdragons/yiuServer/service/MainServiceTest.java
@@ -5,43 +5,34 @@ import devdragons.yiuServer.domain.state.UserEntranceCategory;
 import devdragons.yiuServer.domain.state.UserRoleCategory;
 import devdragons.yiuServer.domain.state.UserStatusCategory;
 import devdragons.yiuServer.domain.state.UserTrackCategory;
+import devdragons.yiuServer.dto.LoginDto;
 import devdragons.yiuServer.dto.request.UserRequestDto;
 import devdragons.yiuServer.exception.CustomException;
 import devdragons.yiuServer.exception.ErrorCode;
 import devdragons.yiuServer.repository.UserRepository;
-import jakarta.mail.internet.MimeMessage;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import static org.mockito.Mockito.*;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MainServiceTest {
-    @InjectMocks
+
+    @Autowired
     private MainService mainService;
 
-    @Mock
+    @Autowired
     private UserRepository userRepository;
-
-    @Mock
-    private JavaMailSender javaMailSender;
-
-    @Mock
-    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void setUp() {
@@ -70,17 +61,17 @@ class MainServiceTest {
 
         // then
         Optional<User> user = userRepository.findById(userRequestDto.getId());
-        assertThat(user.isPresent());
+        assertThat(user).isPresent();
     }
 
     @Test
     @DisplayName("회원가입 실패 테스트 - 데이터 미입력")
-    void 회원가입_실패_데이터_미입력() throws Exception {
+    void 회원가입_실패_데이터_미입력() {
         // given
         UserRequestDto userRequestDto = new UserRequestDto();
 
         // when & then
-        CustomException customException = assertThrows(CustomException.class, () -> {mainService.register(userRequestDto);});
+        CustomException customException = assertThrows(CustomException.class, () -> mainService.register(userRequestDto));
         assertEquals(ErrorCode.INSUFFICIENT_DATA, customException.getErrorCode());
         assertEquals(400, customException.getErrorCode().getStatus());
     }
@@ -89,35 +80,27 @@ class MainServiceTest {
     @DisplayName("회원가입 실패 테스트 - 데이터 중복")
     void 회원가입_실패_데이터_중복() throws Exception {
         // given
-        UserRequestDto userRequestDto1 = new UserRequestDto();
-        userRequestDto1.setId("202033009");
-        userRequestDto1.setName("김예서");
-        userRequestDto1.setPwd("1234");
-        userRequestDto1.setGrade(1);
-        userRequestDto1.setRole(UserRoleCategory.ADMIN);
-        userRequestDto1.setStatus(UserStatusCategory.STUDENT);
-        userRequestDto1.setMajor("33");
-        userRequestDto1.setDepartment("컴퓨터과학");
-        userRequestDto1.setTrack(UserTrackCategory.SINGLE);
-        userRequestDto1.setEntrance(UserEntranceCategory.FRESH);
-        userRequestDto1.setProfessor("이완주");
+        UserRequestDto userRequestDto = new UserRequestDto();
+        userRequestDto.setId("202033009");
+        userRequestDto.setName("김예서");
+        userRequestDto.setPwd("1234");
+        userRequestDto.setGrade(1);
+        userRequestDto.setRole(UserRoleCategory.ADMIN);
+        userRequestDto.setStatus(UserStatusCategory.STUDENT);
+        userRequestDto.setMajor("33");
+        userRequestDto.setDepartment("컴퓨터과학");
+        userRequestDto.setTrack(UserTrackCategory.SINGLE);
+        userRequestDto.setEntrance(UserEntranceCategory.FRESH);
+        userRequestDto.setProfessor("이완주");
 
-        // 첫 번째 회원가입 시에는 중복이 없으므로 false
-        when(userRepository.existsById(userRequestDto1.getId())).thenReturn(false);
-        mainService.register(userRequestDto1);
-
-        // 두 번째 회원가입 시에는 중복이 발생해야 하므로 true로 설정
-        when(userRepository.existsById(userRequestDto1.getId())).thenReturn(true);
+        mainService.register(userRequestDto);
 
         // when & then
-        CustomException customException = assertThrows(CustomException.class, () -> {
-            mainService.register(userRequestDto1);
-        });
+        CustomException customException = assertThrows(CustomException.class, () -> mainService.register(userRequestDto));
 
         assertThat(customException.getErrorCode()).isEqualTo(ErrorCode.DUPLICATE);
         assertThat(customException.getErrorCode().getStatus()).isEqualTo(409);
     }
-
 
     @Test
     @DisplayName("회원가입 시 이메일 전송 테스트")
@@ -125,14 +108,88 @@ class MainServiceTest {
         // given
         String id = "202033009";
 
-        MimeMessage mimeMessage = mock(MimeMessage.class);
-        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
-
         // when
         int authNum = mainService.sendEmailWhenRegister(id);
 
         // then
         assertThat(authNum).isGreaterThanOrEqualTo(100000).isLessThanOrEqualTo(999999);
-        verify(javaMailSender, times(1)).send(any(MimeMessage.class));
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경 시 이메일 전송 테스트")
+    void 비밀번호_변경_시_이메일_전송_테스트() throws Exception {
+        // given
+        UserRequestDto userRequestDto = new UserRequestDto();
+        userRequestDto.setId("202033009");
+        userRequestDto.setName("김예서");
+        userRequestDto.setPwd("1234");
+        userRequestDto.setGrade(1);
+        userRequestDto.setRole(UserRoleCategory.ADMIN);
+        userRequestDto.setStatus(UserStatusCategory.STUDENT);
+        userRequestDto.setMajor("33");
+        userRequestDto.setDepartment("컴퓨터과학");
+        userRequestDto.setTrack(UserTrackCategory.SINGLE);
+        userRequestDto.setEntrance(UserEntranceCategory.FRESH);
+        userRequestDto.setProfessor("이완주");
+
+        mainService.register(userRequestDto);
+
+        // when
+        int authNum = mainService.sendEmailWhenPwdChange(userRequestDto.getId());
+
+        // then
+        assertThat(authNum).isGreaterThanOrEqualTo(100000).isLessThanOrEqualTo(999999);
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경 시 회원 미존재 오류")
+    void 비밀번호_변경_시_회원_미존재_오류() {
+        // given
+        String id = "202033009";
+
+        // when & then
+        CustomException customException = assertThrows(CustomException.class, () -> mainService.sendEmailWhenPwdChange(id));
+
+        assertThat(customException.getErrorCode()).isEqualTo(ErrorCode.NOT_EXIST_ID);
+        assertThat(customException.getErrorCode().getStatus()).isEqualTo(404);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    void 로그인_성공_테스트() throws Exception {
+        // given
+        UserRequestDto userRequestDto = new UserRequestDto();
+        userRequestDto.setId("202033009");
+        userRequestDto.setName("김예서");
+        userRequestDto.setPwd("1234");
+        userRequestDto.setGrade(1);
+        userRequestDto.setRole(UserRoleCategory.ADMIN);
+        userRequestDto.setStatus(UserStatusCategory.STUDENT);
+        userRequestDto.setMajor("33");
+        userRequestDto.setDepartment("컴퓨터과학");
+        userRequestDto.setTrack(UserTrackCategory.SINGLE);
+        userRequestDto.setEntrance(UserEntranceCategory.FRESH);
+        userRequestDto.setProfessor("이완주");
+
+        mainService.register(userRequestDto);
+
+        // when
+        LoginDto loginDto = mainService.login(userRequestDto);
+
+        // then
+        LoginDto expectedLoginDto = LoginDto.builder()
+                .id("202033009")
+                .name("김예서")
+                .grade(1)
+                .role(UserRoleCategory.ADMIN)
+                .status(UserStatusCategory.STUDENT)
+                .major("33")
+                .department("컴퓨터과학")
+                .track(UserTrackCategory.SINGLE)
+                .entrance(UserEntranceCategory.FRESH)
+                .token(loginDto.getToken())  // 토큰 값은 직접 비교하지 않고 그대로 사용
+                .build();
+
+        assertThat(loginDto).isEqualTo(expectedLoginDto);
     }
 }
