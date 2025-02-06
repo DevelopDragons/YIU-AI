a4b2c92f3e2088bae2486719026b6ed661623f92
yeseokim <callikys@naver.com>
2025-02-06 22:43:39 +0900

    feat: mainController 로직 작성

--- src/main/java/devdragons/yiuServer/controller/MainController.java
+++ src/main/java/devdragons/yiuServer/controller/MainController.java
@@ -17,7 +17,31 @@ public class MainController {
 
     // 회원가입
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ResponseEntity<Boolean> join(UserRequestDto request) throws Exception {
+    public ResponseEntity<Boolean> register(UserRequestDto request) throws Exception {
         return new ResponseEntity<Boolean>(mainService.register(request), HttpStatus.OK);
+    }
+
+    // 회원가입 시 이메일 인증
+    @PostMapping(value = "/register/email", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Integer> sendEmailWhenRegister(UserRequestDto request) throws Exception {
+        return new ResponseEntity<>(mainService.sendEmailWhenRegister(request.getId()), HttpStatus.OK);
+    }
+
+    // 비밀번호 변경 시 이메일 인증
+    @PostMapping(value = "/pwd/email", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Integer> sendEmailWhenPwdChange(UserRequestDto request) throws Exception {
+        return new ResponseEntity<>(mainService.sendEmailWhenPwdChange(request.getId()), HttpStatus.OK);
+    }
+
+    // 비밀번호 변경
+    @PostMapping(value = "/pwd/change", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Boolean> changePwd(UserRequestDto request) throws Exception {
+        return new ResponseEntity<>(mainService.changePwd(request), HttpStatus.OK);
+    }
+
+    // 로그인
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<LoginDto> login(UserRequestDto request) throws Exception {
+        return new ResponseEntity<>(mainService.login(request), HttpStatus.OK);
     }
 }
