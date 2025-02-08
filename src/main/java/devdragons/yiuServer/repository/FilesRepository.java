570a13145e1630a7f4f9d540f5f4c16f5754009e
yeseokim <callikys@naver.com>
2025-02-09 01:40:19 +0900

    feat: 다중 파일 저장 관련 로직 작성

---
+++ src/main/java/devdragons/yiuServer/repository/FilesRepository.java
@@ -0,0 +1,9 @@
+package devdragons.yiuServer.repository;
+
+import devdragons.yiuServer.domain.Files;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilesRepository extends JpaRepository<Files, Integer> {
+}
