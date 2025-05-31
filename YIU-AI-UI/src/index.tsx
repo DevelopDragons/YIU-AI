053986f50d72031d4552f0cd27d5d9dafc37f818
MeehyunByeon <bmh2038@naver.com>
2025-05-31 22:06:29 +0900

    test
    Closed #2

--- YIU-AI-UI/src/index.tsx
+++ YIU-AI-UI/src/index.tsx
@@ -28,3 +28,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 테스트 입니다.
